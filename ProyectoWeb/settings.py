ad029e685124c65564a5819b6b76c0418aaf1a3a
Edelan <maximilianolf@hotmail.com>
2023-10-19 22:41:10 +0200

    Muchas cosas pendientes por arreglar

--- ProyectoWeb/settings.py
+++ ProyectoWeb/settings.py
@@ -128,3 +128,10 @@ MEDIA_ROOT : BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.email.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'alfyo89@gmail.com'
+EMAIL_HOST_PASSWORD = '*Maxi34170798'
--- contacto/views.py
+++ contacto/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import FormularioContacto
+from django.core.mail import EmailMessage
 
 # Create your views here.
 
@@ -14,6 +15,17 @@ def contacto(request):
             email = request.POST.get("email")
             contenido = request.POST.get("contenido")
 
-            return redirect("/contacto/?correctopolis")
+            email = EmailMessage("Mensaje desde la APP de Django",
+                                 "El usuario con el nombre {} de email {} envia el siguiente mensaje:\n\n {}".format(nombre, email, contenido),
+                                 "",["alfyo89@gmail.com"], reply_to=[email])
+            
+            try:
+                email.send()
+
+                return redirect("/contacto/?correctopolis")
+            
+            except:
+                
+                return redirect("/contacto/?posNoFunco")
 
     return render(request, "contacto/contacto.html", {'miFormulario': formulario_contacto})
