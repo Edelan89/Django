016071b87f1b0f2100dbd8e3984ac93f54772d8d
Edelan <maximilianolf@hotmail.com>
2023-11-17 17:29:24 +0100

    Update 17-11-23

--- autenticacion/views.py
+++ autenticacion/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect  
 from django.views.generic import View
-from django.contrib.auth import login
+from django.contrib.auth import login, logout
 from django.contrib import messages
 
 from django.contrib.auth.forms import UserCreationForm
@@ -29,3 +29,8 @@ class VRegistro(View):
                 messages.error(request, form.error_messages[msg])
             
             return render(request, "registro/registro.html", {"form" : form})
+        
+def cerrar_sesion(request):
+    logout(request)
+
+    return redirect('Home')
